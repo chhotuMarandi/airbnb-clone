ac453468802549e0ee7f0486cd4a12f17bc51bf1
chhotu marandi <cmarandi29@gmail.com>
2023-05-28 13:06:31 +0530

    added app js component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header'
+
+function App () {
+ return (
+  <div>
+   <h1>Hello Bro</h1>
+   <Header />
+  </div>
+ )
+}
+
+export default App
