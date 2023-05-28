40c0aa6d7015d54161cfb1e825893d045289194c
chhotu marandi <cmarandi29@gmail.com>
2023-05-28 13:23:35 +0530

    added header component JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,18 @@ import logo from '../logo/airbnb-logo.png'
 
 function Header () {
  return (
-  <div>
-   <div className="logo">
-    <img src={logo} alt="" width={100}/>
+   <div>
+     <div className='logo'>
+       <img src={logo} alt='' width={100} />
+     </div>
+     <div className='search-section'>
+       <p>anywhere</p>
+       <p>any week</p>
+       <p>add guests</p>
+       <i class='fa-solid fa-magnifying-glass'></i>
+     </div>
    </div>
-  </div>
- )
+ );
 }
 
 export default Header
