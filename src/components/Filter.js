878a771cd9d138935bafa2e049dfa7ee4bd5c4ce
chhotu marandi <cmarandi29@gmail.com>
2023-06-01 12:01:33 +0530

    add more filter icons

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,64 +4,88 @@ function Filter () {
  return (
    <div className='filter'>
      <div className='filter-item'>
-       <i class='fa-solid fa-house'></i>
+       <i className='fa-solid fa-house'></i>
        <label htmlFor='#'>house</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-hospital'></i>
+       <i className='fa-solid fa-hospital'></i>
        <label htmlFor='#'>hospital</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-truck-front'></i>
+       <i className='fa-solid fa-truck-front'></i>
        <label htmlFor='#'>truck</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-shower'></i>
+       <i className='fa-solid fa-shower'></i>
        <label htmlFor='#'>shower</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-church'></i>
+       <i className='fa-solid fa-church'></i>
        <label htmlFor='#'>church</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-truck-medical'></i>
+       <i className='fa-solid fa-truck-medical'></i>
        <label htmlFor='#'>medical</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-shield-dog'></i>
+       <i className='fa-solid fa-shield-dog'></i>
        <label htmlFor='#'>dog</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-road-barrier'></i>
+       <i className='fa-solid fa-road-barrier'></i>
        <label htmlFor='#'>road</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-plant-wilt'></i>
+       <i className='fa-solid fa-plant-wilt'></i>
        <label htmlFor='#'>plant</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-oil-well'></i>
+       <i className='fa-solid fa-oil-well'></i>
        <label htmlFor='#'>oil</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-microscope'></i>
+       <i className='fa-solid fa-microscope'></i>
        <label htmlFor='#'>microscope</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-lungs'></i>
+       <i className='fa-solid fa-lungs'></i>
        <label htmlFor='#'>lungs</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-mask-face'></i>
+       <i className='fa-solid fa-mask-face'></i>
        <label htmlFor='#'>mask</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-house-signal'></i>
+       <i className='fa-solid fa-house-signal'></i>
        <label htmlFor='#'>signal</label>
      </div>
      <div className='filter-item'>
-       <i class='fa-solid fa-faucet-drip'></i>
+       <i className='fa-solid fa-faucet-drip'></i>
        <label htmlFor='#'>faucet</label>
+     </div>
+     <div className='filter-item'>
+       <i class='fa-solid fa-square-parking'></i>
+       <label htmlFor='#'>parking</label>
+     </div>
+     <div className='filter-item'>
+       <i class='fa-solid fa-tree'></i>
+       <label htmlFor='#'>tree</label>
+     </div>
+     <div className='filter-item'>
+       <i class='fa-solid fa-bridge'></i>
+       <label htmlFor='#'>bridge</label>
+     </div>
+     <div className='filter-item'>
+       <i class='fa-solid fa-bus'></i>
+       <label htmlFor='#'>bus</label>
+     </div>
+     <div className='filter-item'>
+       <i class='fa-solid fa-fax'></i>
+       <label htmlFor='#'>fax</label>
+     </div>
+     <div className='filter-item'>
+       <i class='fa-solid fa-computer'></i>
+       <label htmlFor='#'>computer</label>
      </div>
    </div>
  );
