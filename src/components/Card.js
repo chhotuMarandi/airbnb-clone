ccd5c2106fc9e92dd0cb7a504b90faa689224c6e
chhotu marandi <cmarandi29@gmail.com>
2023-06-02 11:17:01 +0530

    style image section

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,13 @@ function Card () {
  return (
    <div className='card-section'>
      <section className='card'>
-       <img
-         src='https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-         width={400}
-         alt=''
-       />
+       <div className='image-section'>
+         <img
+           src='https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+           alt=''
+         />
+         <i class='fa-regular fa-heart'></i>
+       </div>
        <h4 className='location'>lonavla,india</h4>
        <p className='distance'>63 kilometers away</p>
        <p className='date'>11-16 jun</p>
