5b55a0c35e5a0eaf151b6897e9db7dcec699ffcf
chhotu marandi <cmarandi29@gmail.com>
2023-06-02 14:59:16 +0530

    add Data

---
+++ src/components/Data.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    image:
+      "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    location: "lonavla, India",
+    distance: "53 kilometers away",
+    date: "12 jun - 17 jun",
+    price: "₹9600",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    location: "lonavla, India",
+    distance: "53 kilometers away",
+    date: "12 jun - 17 jun",
+    price: "₹9600",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    location: "goa, India",
+    distance: "64 kilometers away",
+    date: "29 sep - 7 oct",
+    price: "₹6600",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1134176/pexels-photo-1134176.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    location: "pune, India",
+    distance: "88 kilometers away",
+    date: "1 apr - 5 apr",
+    price: "₹8600",
+  },
+
+];
