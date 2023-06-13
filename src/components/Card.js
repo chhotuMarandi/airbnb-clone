be6fcce75652b579098b65b19f0c6a39c56eb7ad
chhotu marandi <cmarandi29@gmail.com>
2023-06-13 17:54:20 +0530

    pass props in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,17 @@
 import React from 'react'
 
-function Card () {
+function Card (props) {
  return (
      <section className='card'>
        <div className='image-section'>
          <img
-           src='https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-           alt=''
-         />
+           src= {props.image}/>
          <i class='fa-regular fa-heart'></i>
        </div>
-       <h4 className='location'>lonavla,india</h4>
-       <p className='distance'>63 kilometers away</p>
-       <p className='date'>11-16 jun</p>
-       <p className='price'>₹9600</p>
+       <h4 className='location'>{props.location}</h4>
+      <p className='distance'>{props.distance}</p>
+      <p className='date'>{props.date}</p>
+       <p className='price'>{props.price}</p>
        <p>night</p>
      </section>
  );
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -4,17 +4,13 @@ import Card from './Card'
 function CardSection() {
   return (
     <div className='card-section'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      
+      <Card
+        image='https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        location='benglore,india'
+        distance='20km away'
+        date='12 jun'
+        price='$343'
+      />
     </div>
   );
 }
